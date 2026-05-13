143e9d54c30a0b7b8bad0f4897c92ad2720562fc
Tales Bernardes Lima <talestrom99@hotmail.com>
2026-05-13 00:22:50 -0300

    [ADD] - Mecânica tiro

--- scripts/carnage.gd
+++ scripts/carnage.gd
@@ -2,8 +2,14 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-# Crescimento de 10% ao colidir com inimigo
-const FATOR_CRESCIMENTO = 1.1
+const FATOR_CRESCIMENTO = 1.1 # Crescimento de 10% ao colidir com inimigo
+const FATOR_REDUCAO = 0.95 # Reduzir 5% é o mesmo que multiplicar por 0.95
+const ESCALA_MAX = 2.5
+const ESCALA_MIN = 0.5
+
+# Referência para o Projétil (Arraste sua cena de tiro para cá no Inspetor)
+@export var projetil_cena: PackedScene
+
 var super_pulo = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -44,6 +50,23 @@ func atualizar_animacao(direction):
 	else:
 		_animated_sprite.play("idle")
 
+# Função responsável pelo tiro do Carnage (e diminuição do tamanho)
+func atirar_e_encolher():
+	# 1. Instancia o projétil
+	if projetil_cena:
+		var tiro = projetil_cena.instantiate()
+		# Define a posição inicial do tiro
+		tiro.global_position = global_position
+		# Define a direção (baseado no flip do sprite)
+		tiro.direction = -1 if _animated_sprite.flip_h else 1
+		get_parent().add_child(tiro)
+
+	# 2. Calcula e limita a nova escala (Mínimo de 0.5)
+	var nova_escala = scale.x * FATOR_REDUCAO
+	nova_escala = max(nova_escala, ESCALA_MIN)
+	
+	scale = Vector2(nova_escala, nova_escala)
+
 # Função responsável pela destruição de inimigo e ativação da função de crescimento
 func _on_body_entered(body: Node2D):
 	if body.is_in_group("Inimigos"):
@@ -61,13 +84,18 @@ func crescer_carnage():
 	# Pegamos a altura antiga antes de crescer
 	var altura_antiga = $CollisionShape2D.shape.get_rect().size.y * scale.y
 	
-	# Aplica o crescimento
-	scale *= FATOR_CRESCIMENTO
+	# Calcula nova escala com limite máximo de 2.5 em relação ao tamanho original
+	var nova_escala = scale.x * FATOR_CRESCIMENTO
+	nova_escala = min(nova_escala, ESCALA_MAX)
+	
+	# Atualiza a escala
+	scale = Vector2(nova_escala, nova_escala)
 	
 	# Pegamos a nova altura
 	var nova_altura = $CollisionShape2D.shape.get_rect().size.y * scale.y
 	
 	# Calculamos a diferença e subimos o personagem exatamente essa diferença
+	# Para que ele cresça a partir dos pés
 	global_position.y -= (nova_altura - altura_antiga) / 2
 
 func _on_animated_sprite_2d_animation_looped() -> void:
--- scripts/projetil.gd
+++ scripts/projetil.gd
@@ -1,5 +1,10 @@
 extends Area2D
-var velocidade = -400
+
+var velocidade = 500
+var direcao = 1
 
 func _process(delta):
 	position.x += velocidade * delta
+	
+func _on_screeexited():
+	queue_free()
