9112c5afa2902379a83430c6f791113ffb9ee7ba
Tales Bernardes Lima <talestrom99@hotmail.com>
2026-06-15 15:33:36 -0300

    [FIX] - Correção de bugs

--- scripts/projetil.gd
+++ scripts/projetil.gd
@@ -9,16 +9,22 @@ func _process(delta: float) -> void:
 	
 # Função responsável pela colisão com inimigos
 func _on_area_entered(area: Node2D) -> void:
-	# Se bater em um, inimigo, aplica lógica de dano
+	print("COLIDI COM:", area.name)
+	print("GRUPOS:", area.get_groups())
+	
 	if area.is_in_group("inimigos"):
 		area.queue_free()
-		
-	if area.is_in_group("Quebravel"):
-		area.quebrar()
-		# Destrói o projétil
 		queue_free()
 
-# Destrói o projétil uma vez que ele não esteja mais visível
+	if area.is_in_group("Quebravel"):
+		var parede = area.get_parent()
+		
+		print("PAI:", parede.name)
+		print("TEM QUEBRAR:", parede.has_method("quebrar"))
+		
+		if parede.has_method("quebrar"):
+			parede.quebrar()
+			queue_free()
+
 func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
-	queue_free()
-	
+	queue_free()	
--- scripts/quebrável.gd
+++ scripts/quebrável.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 @onready var sprite = $AnimatedSprite2D
 
